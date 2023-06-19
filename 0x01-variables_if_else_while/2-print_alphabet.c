a1b7785da55e834626508ac352f2844bc6016236
merytaa <taalite.meriam@gmail.com>
2023-06-18 17:40:29 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+	while (n <= 122)
+	{
+		putchar(n);
+		putchar('\n');
+		n++;
+	}
+	return (0);
+}
