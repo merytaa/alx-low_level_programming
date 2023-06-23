c28de418b465da49335d214b1a2bc3db88be72b9
merytaa <taalite.meriam@gmail.com>
2023-06-22 18:06:18 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,7 @@ print_most_numbers(void)
 {
 	int x = 0;
 
-	for (; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		if (x == 2 || x == 4)
 		{
@@ -18,7 +18,7 @@ print_most_numbers(void)
 		}
 		else
 		{
-			_putchar(x + '0')
+			_putchar(x + '0');
 		}
 	}
 	_putchar('\n');
