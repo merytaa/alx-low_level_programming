51a647d96bbf53ee1b0f605ce0ddaa1bae4e88f7
merytaa <taalite.meriam@gmail.com>
2023-06-22 18:21:18 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a stright line according to param
+ * @n : The number of lines to draw
+ * Return: empty
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
