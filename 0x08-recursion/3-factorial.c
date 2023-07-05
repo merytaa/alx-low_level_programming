9fd74639c280bf174b1793336484e6d9acbaee84
merytaa <taalite.meriam@gmail.com>
2023-07-05 14:48:03 -0700

    0x08 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - check the code
+ * @i: number
+ * Return: f of n
+ */
+int factorial(int i)
+{
+	if (i < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
