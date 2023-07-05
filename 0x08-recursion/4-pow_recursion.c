bd7e453b48ae106be43ebd34660966758d73301a
merytaa <taalite.meriam@gmail.com>
2023-07-05 15:11:30 -0700

    0x08 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,9 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * _pow_recursion - check the code
+ * @x: value
+ * @y: value
  * Return: Always 0.
  */
-int main(void)
+int _pow_recursion(int x, int y)
 {
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
