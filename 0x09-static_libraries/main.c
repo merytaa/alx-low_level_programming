b34ebedd39462d94b12c2aaab240983b832b96ef
merytaa <taalite.meriam@gmail.com>
2023-07-12 13:58:21 -0700

    lol

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - writes the character c to stdout
+ * Return: On success 1.
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t - Kevin Mitnick");
+	return(0);
+}
